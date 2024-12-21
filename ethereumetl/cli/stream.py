4abc58f0a9966ecbd8b658632695f0f4c7f99b93
Eren Sanlıer <erensanlier@gmail.com>
2024-12-21 13:26:35 +0300

    feat: Add end-block option to stream command for enhanced block range control

--- ethereumetl/cli/stream.py
+++ ethereumetl/cli/stream.py
@@ -44,9 +44,10 @@ from ethereumetl.thread_local_proxy import ThreadLocalProxy
                    'or kafka, output name and connection host:port e.g. kafka/127.0.0.1:9092 '
                    'or Kinesis, e.g. kinesis://your-data-stream-name'
                    'If not specified will print to console')
-@click.option('--output-config-file', default='output_config.yaml', type=str,
+@click.option('--output-config-file', default=None, type=str,
               help='Configuration for the output. File path. For Kafka, YAML file with KafkaProducer configurations (will override bootstrap_servers if included).')
 @click.option('-s', '--start-block', default=None, show_default=True, type=int, help='Start block')
+@click.option('-f', '--end-block', default=None, show_default=True, type=int, help='End block')
 @click.option('-e', '--entity-types', default=','.join(EntityType.ALL_FOR_INFURA), show_default=True, type=str,
               help='The list of entity types to export.')
 @click.option('--period-seconds', default=10, show_default=True, type=int, help='How many seconds to sleep between syncs')
@@ -55,7 +56,7 @@ from ethereumetl.thread_local_proxy import ThreadLocalProxy
 @click.option('-w', '--max-workers', default=5, show_default=True, type=int, help='The number of workers')
 @click.option('--log-file', default=None, show_default=True, type=str, help='Log file')
 @click.option('--pid-file', default=None, show_default=True, type=str, help='pid file')
-def stream(last_synced_block_file, lag, provider_uri, output, output_config_file, start_block, entity_types,
+def stream(last_synced_block_file, lag, provider_uri, output, output_config_file, start_block, end_block, entity_types,
            period_seconds=10, batch_size=2, block_batch_size=10, max_workers=5, log_file=None, pid_file=None):
     """Streams all data types to console or Google Pub/Sub."""
     configure_logging(log_file)
@@ -84,6 +85,7 @@ def stream(last_synced_block_file, lag, provider_uri, output, output_config_file
         last_synced_block_file=last_synced_block_file,
         lag=lag,
         start_block=start_block,
+        end_block=end_block,
         period_seconds=period_seconds,
         block_batch_size=block_batch_size,
         pid_file=pid_file
